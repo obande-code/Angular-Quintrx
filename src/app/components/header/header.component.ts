b0e634a10fdd3ab76d861c32af8b5f8ebb313dd3
obande-code <jamesobande2015@gmail.com>
2021-11-05 17:47:42 +0800

    no message

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -20,16 +20,16 @@ export class HeaderComponent implements OnInit {
   tokenData$: Observable<TokenData>;
   menuData$: Observable<any>;
   menus = [];
-  list: FaqItem[] = [
-    {
-          question: 'Why does it think the jQuery plugin is missing?',
-          answer: 'Remember: load jQuery before AngularJS if you are using jQuery plugins!'
-        },
-        {
-          question: 'How do I access the DOM from a controller?',
-          answer: 'DO NOT perform DOM selection/traversal from the controller. The HTML hasn\'t rendered yet. Look up \'directives\'.'
-        }
-  ];
+  // list: FaqItem[] = [
+  //   {
+  //         question: 'Why does it think the jQuery plugin is missing?',
+  //         answer: 'Remember: load jQuery before AngularJS if you are using jQuery plugins!'
+  //       },
+  //       {
+  //         question: 'How do I access the DOM from a controller?',
+  //         answer: 'DO NOT perform DOM selection/traversal from the controller. The HTML hasn\'t rendered yet. Look up \'directives\'.'
+  //       }
+  // ];
   //showMenu = false;
 
 
